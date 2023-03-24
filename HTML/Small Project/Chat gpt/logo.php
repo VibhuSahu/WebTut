21ff1c9b68bbe5b86d9ba782448221cc40c75dfa
Vibhu <thevibhusahu2003@gmail.com>
2023-03-25 02:32:54 +0530

    0001 First commit

---
+++ HTML/Small Project/Chat gpt/logo.php
@@ -0,0 +1,8 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+  <rect width="100%" height="100%" fill="#fff"/>
+  <circle cx="100" cy="100" r="80" fill="#ffcc00"/>
+  <rect x="40" y="70" width="40" height="60" fill="#333"/>
+  <rect x="120" y="70" width="40" height="60" fill="#333"/>
+  <rect x="70" y="50" width="60" height="100" fill="#333"/>
+  <rect x="80" y="70" width="40" height="60" fill="#ffcc00"/>
+</svg>
---
+++ PHP/chapter1(variables and basics)/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<body>
+    <div class="container">
+        This is my first php website.
+    </div>
+
+
+
+
+    <!-- 
+        PHP is a server-side programming language, meaning it is executed at the web server before the website is sent to the end-user(you).
+        This is why you can't see the PHP code when you view the source code.
+
+     -->
+    <?php
+
+        // echo help you to you to output one or more string 
+        echo "Hello world! and this is printed using PHP";
+
+
+        // Single line comment 
+        /*
+            This 
+            is
+            a 
+            multi-line 
+            comment
+        */
+
+
+        // Variables 
+        $variable1 = 5;
+        $variable2 = 2;
+        echo $variable1;
+        echo $variable2;
+    ?>
+
+
+    
+</body>
+</html>
